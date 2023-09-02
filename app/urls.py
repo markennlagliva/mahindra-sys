4e1a3b7e13878f0c5dca10dc226e39b4dce028e7
markennlagliva <markennlagliva@gmail.com>
2023-09-02 15:19:31 +0800

    small changes

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('administrator/', views.administrator, name='administrator'),
+    path('employee/', views.employee, name='employee'),
 ]
