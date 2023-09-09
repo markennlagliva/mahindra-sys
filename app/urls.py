9c18472203ac0f031317d79f05e169856fab2e06
markennlagliva <markennlagliva@gmail.com>
2023-09-09 11:51:03 +0800

    navbar responsive, urls settled up

--- app/urls.py
+++ app/urls.py
@@ -10,6 +10,8 @@ urlpatterns = [
     path('admin_dashboard/', views.admin_dashboard, name='admin_dashboard'),
     path('employee_dashboard/', views.employee_dashboard, name='employee_dashboard'),
 
-    path('register_admin/', views.registeradmin, name='register_admin'),
-    path('register_employee/', views.registeremployee, name='register_employee'),
+    # ADMIN DASHBOARD
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('register_admin/', views.register_admin, name='register_admin'),
+    path('register_employee/', views.register_employee, name='register_employee'),
 ]
